0d417d10f9a26764cbef59662d1510409d0a551d
moduhan <devduhanmo@gmail.com>
2021-11-23 16:26:56 +0900

    feat: 프록시 패턴 - 예제

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/code/ProxyPatternClient.java
@@ -0,0 +1,17 @@
+package hello.proxy.pureproxy.proxy.code;
+
+/**
+ * Subject 인터페이스에 의존하고, Subject를 호출하는 클라이언트
+ */
+public class ProxyPatternClient {
+
+    private Subject subject;
+
+    public ProxyPatternClient(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void execute() {
+        subject.operation();
+    }
+}
